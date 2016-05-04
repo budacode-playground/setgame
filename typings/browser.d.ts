b568681c0add113096273f26d016d6acfe312f5f
Attila Olah <NoNameProvided@users.noreply.github.com>
2016-05-04 18:16:03 +0200

    Remove typings folder

--- typings/browser.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="browser/ambient/es6-collections/index.d.ts" />
-/// <reference path="browser/ambient/es6-promise/index.d.ts" />
-/// <reference path="browser/ambient/lodash/index.d.ts" />
-/// <reference path="browser/ambient/require/index.d.ts" />
--- typings/main.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="main/ambient/es6-collections/index.d.ts" />
-/// <reference path="main/ambient/es6-promise/index.d.ts" />
-/// <reference path="main/ambient/lodash/index.d.ts" />
-/// <reference path="main/ambient/require/index.d.ts" />
